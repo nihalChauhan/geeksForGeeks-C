95511e145e1440ae94b588fd91caf47e7e65dd60
Nihal Chauhan <nihalchauhan16@gmail.com>
2017-06-25 13:03:47 +0530

    selection sort

---
+++ Searching_And_Sorting/Sorting/selection_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void swap(int *x, int *y);
+
+int main()
+{
+  int a[20], n, i, j, min_i;
+  //Enter Number of Elements
+  scanf("%d", &n);
+
+  //Enter Sorted Elements
+  for(i=0;i<n;i++)
+    scanf("%d", &a[i]);
+
+  for (i = 0; i < n-1; i++)
+  {
+    min_i = i;
+    for (j = i+1; j < n; j++)
+      if (a[j] < a[min_i])
+        min_i = j;
+
+    swap(&a[min_i], &a[i]);
+  }
+
+  printf("Sorted array:");
+
+  for (i=0; i < n; i++)
+    printf(" %d", a[i]);
+
+  return 0;
+}
+
+void swap(int *x, int *y)
+{
+  int t = *x;
+  *x = *y;
+  *y = t;
+}
