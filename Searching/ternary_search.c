45f5d52a21dd6b8152109799534d923cc9961162
Nihal Chauhan <nihalchauhan16@gmail.com>
2017-06-25 12:43:17 +0530

    ternary search

--- Searching/ternary_search.c
+++ Searching/ternary_search.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
-int ternarySearch(int arr[], int l, int r, int x)
-{
-  if (r >= l)
-  {
-    int mid1 = l + (r - l)/3;
-    int mid2 = mid1 + (r - l)/3;
 
-    if (arr[mid1] == x)  return mid1;
-
-    if (arr[mid2] == x)  return mid2;
-
-    if (arr[mid1] > x) return ternarySearch(arr, l, mid1-1, x);
-
-    if (arr[mid2] < x) return ternarySearch(arr, mid2+1, r, x);
-
-    return ternarySearch(arr, mid1+1, mid2-1, x);
-  }
-  return -1;
-}
-
-#include <stdio.h>
+int ternarySearch(int arr[], int l, int r, int x);
 
 int main() {
   int a[20], n, i, k, p;
@@ -38,4 +19,25 @@ int main() {
     printf("Found at : %d\n", p+1);
   else
     printf("Not Found\n");
+}
+
+
+int ternarySearch(int arr[], int l, int r, int x)
+{
+  if (r >= l)
+  {
+    int mid1 = l + (r - l)/3;
+    int mid2 = mid1 + (r - l)/3;
+
+    if (arr[mid1] == x)  return mid1;
+
+    if (arr[mid2] == x)  return mid2;
+
+    if (arr[mid1] > x) return ternarySearch(arr, l, mid1-1, x);
+
+    if (arr[mid2] < x) return ternarySearch(arr, mid2+1, r, x);
+
+    return ternarySearch(arr, mid1+1, mid2-1, x);
+  }
+  return -1;
 }
