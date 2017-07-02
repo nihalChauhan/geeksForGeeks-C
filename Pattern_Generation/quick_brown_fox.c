71b3f022f05e797783044449d5028a83d914fdb5
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-03 02:08:13 +0530

    the qui

---
+++ Pattern_Generation/quick_brown_fox.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+
+void print_t(int n, int i);
+void print_h(int n, int i);
+void print_e(int n, int i);
+
+void print_q(int n, int i);
+void print_u(int n, int i);
+void print_i(int n, int i);
+void print_c(int n, int i);
+void print_k(int n, int i);
+
+
+int main(){
+  int d;
+  scanf("%d",&d);
+  for(int i=0; i<d; i++){
+
+    print_t(d, i);
+
+    printf(" ");
+    print_h(d, i);
+
+    printf(" ");
+    print_e(d, i);
+
+    printf(" ");
+    printf(" ");
+
+    print_q(d, i);
+
+    printf(" ");
+    print_u(d, i);
+
+    printf(" ");
+    print_i(d, i);
+
+    printf("\n");
+  }
+  return 0;
+}
+
+void print_t(int n, int i){
+  n = n%2?n:n-1;
+  for(int j=0; j < n; j++){
+    if(i==0 || j == n/2)
+      printf("*");
+    else
+      printf(" ");
+  }
+}
+
+void print_h(int n, int i){
+  printf("*");
+  for(int j=1; j < n-1; j++){
+    if(i==n/2)
+      printf("*");
+    else
+      printf(" ");
+  }
+  printf("*");
+}
+
+void print_e(int n, int i){
+  printf("*");
+  for(int j=1; j < n; j++){
+    if(i==0 || i==n-1)
+      printf("*");
+    else if(i==n/2){
+      if(j<n/2+1)
+      printf("*");
+      else
+      printf(" ");
+    }
+    else printf(" ");
+  }
+}
+
+void print_q(int n, int i){
+  static int k = 0;
+  int c = 0;
+  printf("*");
+  for(int j=1; j < n-1; j++){
+    if(i==0 || i==n-1)
+      printf("*");
+    else if(i>=2*n/3){
+      if(j==2*n/3+k && c!=1){
+        printf("*");
+        k++;c++;
+      }
+      else
+      printf(" ");
+    }
+    else printf(" ");
+  }
+  printf("*");
+}
+
+void print_u(int n, int i){
+  printf("*");
+  for(int j=1; j < n-1; j++){
+    if(i==n-1)
+      printf("*");
+    else
+      printf(" ");
+  }
+  printf("*");
+}
+
+void print_i(int n, int i){
+  int k = n%2?n:n-1;
+  for(int j=0; j<k; j++){
+    if(i==0 || i==n-1)
+      printf("*");
+    else{
+      if(j==k/2)
+        printf("*");
+      else
+        printf(" ");
+    }
+  }
+}
