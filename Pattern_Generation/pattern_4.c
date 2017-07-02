b45dec4ae3b3ff4bcf7d3bf5844bb1b06bff837d
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-03 00:34:14 +0530

    Pattern C3

---
+++ Pattern_Generation/pattern_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+  int n, i, j;
+  scanf("%d", &n);
+
+  for(i=0; i<n; i++){
+    for(j=0;j<i;j++)
+      printf("0");
+
+    printf("*");
+    for(j=0;j<n-i-1;j++)
+      printf("0");
+
+    printf("*");
+    for(j=0;j<n-i-1;j++)
+      printf("0");
+
+    printf("*");
+    for(j=0;j<i;j++)
+      printf("0");
+    printf("\n");
+  }
+  return 0;
+}
