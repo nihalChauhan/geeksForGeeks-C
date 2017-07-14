0704337d94d336f3b92bd0ae33cdf56bf82b70e2
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-14 23:54:44 +0530

    Minimize Height Range

---
+++ Company_Specific/Adobe/min_max_heights.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int min_max_h(int a[], int n, int k);
+
+int main(){
+  int a[20], n, k, i=0;
+  scanf("%d", &n);
+  for(;i<n; i++)
+    scanf("%d", &a[i]);
+  scanf("%d", &k);
+  printf("%d\n", min_max_h(a, n, k));
+  for(i=0;i<n; i++)
+    printf("%d ", a[i]);
+  printf("\n");
+  return 0;
+}
+
+int abs(int a){ return a<0? a*-1 : a; }
+
+int min_max_h(int a[], int n, int k){
+  int i, min, max, sum;
+  min = max = 0;
+  sum = a[0];
+  for(i=1; i<n; i++){
+    if(a[i]<a[min]) min = i;
+    if(a[i]>a[max]) max = i;
+    sum+=a[i];
+  }
+  if(sum < k*n){
+    for(i=0; i<n; i++)
+      a[i] += k;
+    if(a[max]-a[min] >= k)
+      a[max]-=2*k;
+    return a[max]-a[min];
+  }
+  int mx = a[max];
+  int mn = a[min];
+  max = min = 0;
+  for (i=0; i<n; i++){
+    if(abs((mx+mn) - 2*(a[i]+k))<=abs((mx+mn) - 2*(a[i]-k)))
+      a[i] += k;
+    else
+      a[i] -= k;
+    if(a[i]<a[min]) min = i;
+    if(a[i]>a[max]) max = i;
+  }
+  return a[max]-a[min];
+}
