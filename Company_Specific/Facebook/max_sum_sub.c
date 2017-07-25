1ce4aa0a130c21ed1a2cbff5ccf51ad1ec7cfa2a
nihalChauhan <nihalchauhan16@gmail.com>
2017-07-25 23:59:39 +0530

    Largest sub

---
+++ Company_Specific/Facebook/max_sum_sub.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int find_max(int a[20], int n);
+
+int main(){
+  int a[20], n, i;
+  scanf("%d", &n);
+  for(i=0; i<n; i++)
+    scanf("%d", &a[i]);
+  printf("%d\n", find_max(a, n));
+  return 0;
+}
+
+int find_max_k(int a[20], int n, int k){
+  int i;
+  int max, sum=0;
+  sum = max = a[0];
+  for(i=1; i<n; i++){
+    sum+=a[i];
+    if(sum<0)
+      sum = 0;
+    if(sum>max)
+      max=sum;
+  }
+  return max;
+}
